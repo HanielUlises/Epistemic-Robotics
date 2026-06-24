562e4141877a03480a2ce7548a823538c80be484
HanielUlises <hanielulises2003@gmail.com>
2026-06-23 23:12:27 -0600

    Modularizing, moving to using mu-calculus for path planning support

--- core/planner/include/parser.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "task.hpp"
-#include <string>
-
-// Load a ground epistemic planning task from the JSON file
-PlanningTask load_task(const std::string& json_path);
--- lean/Epistemic/Main.lean
+++
@@ -1,4 +0,0 @@
-import Epistemic
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
